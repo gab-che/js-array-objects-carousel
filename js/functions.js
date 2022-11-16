df4e790174a76a668507e2e60f1199151d422725
gab-che <gabriellachessa1@gmail.com>
2022-11-16 14:54:36 +0100

    initial commit - adds static markup

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+/**
+ * Cambia classi visibilità alle immagini
+ */
+
+// function toggleVisibility(){
+//     const oldActiveImg = imgContainer.querySelector(".visible");
+//     const oldActiveThumb = thumbContainer.querySelector(".visible");
+
+//     oldActiveImg.classList.remove("visible");
+//     oldActiveThumb.classList.remove("visible");
+
+//     const allImgs = imgContainer.querySelectorAll("img");
+//     const allThumbs = thumbContainer.querySelectorAll("img");
+
+//     const newActiveImg = allImgs[currentIndex];
+//     const newThumb = allThumbs[currentIndex];
+
+//     newActiveImg.classList.add("visible");
+//     newThumb.classList.add("visible");
+// }
